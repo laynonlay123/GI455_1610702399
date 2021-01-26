578d7e1b6dfe1b7260c54c0f4f79a8d354784802
LAY\LAY <bomber_lay@hotmail.com>
2021-01-26 16:12:58 +0700

    Send Assignment Week1

---
+++ GI455_Project/Assets/TextMatching/TextMatching.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI ;
+
+public class TextMatching : MonoBehaviour
+{
+    public GameObject Search, Gotit ;
+    public string Word ;
+    public string[] AllName ;
+    
+    public void SearchName()
+    {
+        Word = Search.GetComponent<Text>().text ;
+
+        for (int i = 0; i < AllName.Length ; i++)
+        {
+            if (Word == AllName[i])
+            {
+                Gotit.GetComponent<Text>().text = "[" + Word + "]" + "Yes it have "; break;
+            }
+            else
+            {
+                Gotit.GetComponent<Text>().text = "[" + Word + "]" + "it not have ";
+            }
+        }
+    }
+}
